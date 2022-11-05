cb8b8d91764a6636469de0cec99ce33235748b29
Luan Santos <luanhenriquer8@gmail.com>
2022-11-05 18:33:05 +0000

    add final on repository variable and add consumes and produces on resource

--- src/main/java/com/shockyng/rentacar/api/adapters/api/CarsResource.java
+++ src/main/java/com/shockyng/rentacar/api/adapters/api/CarsResource.java
@@ -5,13 +5,18 @@ import com.shockyng.rentacar.api.adapters.dto.CarDTO;
 import com.shockyng.rentacar.api.domain.entities.Car;
 import com.shockyng.rentacar.api.domain.ports.CarRepository;
 import com.shockyng.rentacar.api.domain.services.cars.CreateNewCar;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/v1/cars")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class CarsResource {
 
-    private CarRepository carRepository = new CarJPARepository();
+    private final CarRepository carRepository = new CarJPARepository();
 
     @POST
     public CarDTO createNewCar(CarDTO carDTO) {
